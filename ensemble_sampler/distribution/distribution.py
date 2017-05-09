44e966efcec73d6587e3643262df678b6a40092c
Jialin Mao <jm5830@nyu.edu>
2017-05-09 00:17:29 -0400

    Revert "Revert "Finished sampler, proposal & distribution. TODO: chain.""
    This reverts commit 091f4527af9d52e2567fa7ef8a55524834d63d34.

--- ensemble_sampler/distribution/distribution.py
+++ ensemble_sampler/distribution/distribution.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Abstract for target distribution. 
+"""
 
 
 class Distribution(object):
-
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         pass
 
     def get_lnprob(self, x):
--- ensemble_sampler/proposal/proposal.py
+++ ensemble_sampler/proposal/proposal.py
@@ -5,9 +5,15 @@ Abstract class for proposals.
 
 class Proposal(object):
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         pass
 
     def propose(self, curr_walker, ensemble):
-        raise NotImplementedError
+        raise NotImplementedError("proposal method must be implemented"
+                                  "by subclass")
 
+    def ln_transition_prob(self, x, y):
+        """
+        Log transition probability from x -> y. Return constant 1.0 if the proposal is symmetric.
+        """
+        return 1.0
