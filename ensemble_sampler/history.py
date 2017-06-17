e7886b1282d18a39ee49e626f9f456d0680e08b7
Jialin Mao <jm5830@nyu.edu>
2017-06-17 20:33:13 +0800

    Remove unnecessary protected properties.

--- ensemble_sampler/history.py
+++ ensemble_sampler/history.py
@@ -2,13 +2,11 @@ from __future__ import division
 
 import numpy as np
 import h5py
-import os.path
+import os
 
 # NOTE: For plotting with seaborn. Can be commented out if do not have package installed.
 import seaborn as sns
 from pandas import DataFrame
-
-from utils import *
 
 
 class History(object):
@@ -27,19 +25,20 @@ class History(object):
         :param save_every: max len of stored history
         :param extra: Dictionary {'blah': dim(blah)} of extra information to store.
         """
-        self._dim = dim
-        self._nwalkers = nwalkers
-        self._niter = niter
-        self._save_every = save_every
+        self.dim = dim
+        self.nwalkers = nwalkers
+        self.niter = niter
+        self.curr_pos = None
+        self.save_fname = ''
+
+        self._max_len = save_every or niter
 
         self._name_to_dim = {'chain': dim}
         self._name_to_dim.update(extra)
 
         self._history = None
-        self._curr_pos = None
-        self._save_fname = ''
 
-        self._history = {k: np.zeros([self._nwalkers, self._save_every, v])
+        self._history = {k: np.zeros([self.nwalkers, self._max_len, v])
                          for k, v in zip(self._name_to_dim.keys(), self._name_to_dim.values())}
 
     def clear(self):
@@ -54,12 +53,13 @@ class History(object):
         Updating the information of _walker_idx_th walker.
         Info is passed in through kwargs in the form name=data
         """
+        itr = itr % self._max_len
         for k in self._history.keys():
             if kwargs.get(k) is not None:
                 self._history[k][walker_idx, itr, :] = kwargs.get(k)
 
     def move(self, new_pos, walker_idx=slice(None)):
-        self._curr_pos[walker_idx] = new_pos
+        self.curr_pos[walker_idx] = new_pos
 
     def save_to(self, save_dir, title):
         """
@@ -71,13 +71,13 @@ class History(object):
             f = h5py.File(self._save_fname, 'r+')
             for name in self._name_to_dim.keys():
                 dset = f[name]
-                dset.resize(dset.shape[1] + self._save_every, axis=1)
-                dset[:, -self._save_every:, :] = self._history.get(name)
+                dset.resize(dset.shape[1] + self._max_len, axis=1)
+                dset[:, -self._max_len:, :] = self._history.get(name)
         else:
             f = h5py.File(self._save_fname, 'w')
             for name in self._name_to_dim.keys():
-                dset = f.create_dataset(name, (self._nwalkers, self._save_every, self._name_to_dim[name]), dtype='f',
-                                        maxshape=(self._nwalkers, self._niter, self._name_to_dim[name]), chunks=True)
+                dset = f.create_dataset(name, (self.nwalkers, self._max_len, self._name_to_dim[name]), dtype='f',
+                                        maxshape=(self.nwalkers, self.niter, self._name_to_dim[name]), chunks=True)
                 dset[...] = self._history.get(name)
         f.close()
 
@@ -95,8 +95,8 @@ class History(object):
                 try:
                     dset = dict([(i, f[i]) for i in idx]) if hdf5 else dict([(i, f[i][::get_every]) for i in idx])
                 except KeyError, err:
-                    print err
                     print 'Supported keys: %s' % str(self._history.keys())
+                    raise err
             return dset
         else:
             if not isinstance(name, list):
@@ -123,21 +123,6 @@ class History(object):
             print err
             print 'Supported keys: %s' % str(self._history.keys())
 
-    def plot_trajectory(self, walker_id, dim, start_from=0):
-        """
-        Plot the trajectory of selected walker in the selected dimension(s).
-        dim should be an integer array.
-        """
-        assert isinstance(walker_id, (int, float)), "Trajectory plotting is supported for SINGLE WALKER only."
-        plot_trajectory(dim, self.get('chain')[walker_id], start_from=start_from)
-
-    def plot_hist(self, dim, start_from=0):
-        """
-        Plot histogram of samples in selected dimension(s). 
-        Samples from walkers in `walker_id` will be stacked to give the final plot.
-        """
-        plot_hist(dim, self.get_flat('chain').T, start_from=start_from)
-
     def plot_scatter(self, dim, kind='kde'):
         """
         Scattered plot for two chosen dimensions. Not sure whether this makes sense...
@@ -151,45 +136,19 @@ class History(object):
 
     @property
     def acceptance_rate(self):
-        return np.sum(self._history.get('accepted'), axis=1) / float(self._niter)
+        return np.sum(self._history.get('accepted'), axis=1) / float(self.niter)
 
     @property
     def history(self):
         return self._history
 
     @property
-    def curr_pos(self):
-        return self._curr_pos
+    def max_len(self):
+        return self._max_len
 
-    @curr_pos.setter
-    def curr_pos(self, p):
-        self._curr_pos = p
-
-    @property
-    def save_fname(self):
-        return self._save_fname
-
-    @save_fname.setter
-    def save_fname(self, name):
-        self._save_fname = name
-
-    @property
-    def niter(self):
-        return self._niter
-
-    @niter.setter
-    def niter(self, N):
-        self._niter = N
-        self._history = {k: np.zeros([self._nwalkers, self._save_every, v])
-                         for k, v in zip(self._name_to_dim.keys(), self._name_to_dim.values())}
-
-    @property
-    def save_every(self):
-        return self._save_every
-
-    @save_every.setter
-    def save_every(self, N):
-        self._save_every = N
-        self._history = {k: np.zeros([self._nwalkers, self._save_every, v])
+    @max_len.setter
+    def max_len(self, N):
+        self._max_len = N
+        self._history = {k: np.zeros([self.nwalkers, self._max_len, v])
                          for k, v in zip(self._name_to_dim.keys(), self._name_to_dim.values())}
 
